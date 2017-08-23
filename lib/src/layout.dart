e9e5606663544be83f456e11262ddb47d3a153f8
Milan Zimmermann <milan.zimmermann@gmail.com>
2017-08-23 00:05:12 -0500

    Added a test class to experiment with this being a library for flutter_charts

--- lib/src/layout.dart
+++ lib/src/layout.dart
@@ -1,6 +1,6 @@
 class Layout {
 
   void printSomething() {
-    print("Hello World from a library package, from GITHUB or local pub repo!");
+    print(" AHA NO PUB GET, Hello World from a library package, from GITHUB or local pub repo!");
   }
 }
